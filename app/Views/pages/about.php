e7da2febbad662c31cf0fedb01cd1b561674ff81
rkynn <rakeyannuramria@gmail.com>
2020-10-31 15:42:36 +0700

    Reupload Sistem Login & Register (lib Myth)

---
+++ app/Views/pages/about.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non quaerat, quibusdam placeat, repudiandae est nulla inventore at quasi molestiae atque exercitationem quos iusto eligendi ratione quod error fuga ad eaque.</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Hello, world!</h1>
+            <p>Selamat datang di ci4login..</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
